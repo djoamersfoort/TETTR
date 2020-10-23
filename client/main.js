897031e8b90fbc26d19d9d6be843498376aa9c4b
Nielsnlamf <nielsnlamf@gmail.com>
2020-10-23 20:13:39 +0200

    Started working on playing field

--- client/main.js
+++ client/main.js
@@ -1,8 +1,21 @@
+const rows = 20;
+const col = column = 20;
+
+let board = [ ];
+
+
 function setup() {
-    createCanvas(400,400);
+    createCanvas(500,550);
+    x = width / 2;
+    for(let r = 0; r < rows; r++) {
+        board[r] = [ ];
+
+        for(let c = 0; c < cols; c++) {
+            board[r][c] = VACANT;
+        }
+    }
 }
 
 function draw() {
-    background(220);
-    ellipse(50, 50, 50);
+    background(150);
 }
